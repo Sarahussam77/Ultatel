d56942df6b5863340942df019e95ea84a88e5879
sara hussam <sarahussam203@gmail.com>
2023-08-12 01:03:29 +0300

    edit validation in nest

--- nest-backend/src/students/dto/create-student.dto/create-student.dto.ts
+++ nest-backend/src/students/dto/create-student.dto/create-student.dto.ts
@@ -4,27 +4,34 @@ import {
   IsDateString,
   IsString,
   Validate,
+  IsNotEmpty,
 } from "class-validator";
 import { Gender } from "../../student.model";
 import { AgeValidation } from "../../decorators/age-validation.decorator";
 
 export class CreateStudentDto {
+  @IsNotEmpty()
   @IsString()
   Fname: string;
 
+  @IsNotEmpty()
   @IsString()
   Lname: string;
 
+  @IsNotEmpty()
   @IsEmail()
   email: string;
 
+  @IsNotEmpty()
   @IsEnum(Gender)
   gender: Gender;
 
+  @IsNotEmpty()
   @IsDateString()
   @Validate(AgeValidation)
   dateOfBirth: Date;
 
+  @IsNotEmpty()
   @IsString()
   country: string;
 }
